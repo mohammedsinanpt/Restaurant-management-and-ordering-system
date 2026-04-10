2fd5ae2f9619be4bb1c941a1fddf36ef9a42677a
Mohammed Sinan <cnasinz@gmail.com>
2026-04-10 22:16:45 +0530

    update api url for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://restaurant-backend-qi8z.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
